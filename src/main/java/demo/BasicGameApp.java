d83b45fc06416152dc346e8ef567755d4bde9b7f
AlmasB <almaslvl@gmail.com>
2017-06-20 12:12:01 +0100

    initial commit

---
+++ src/main/java/demo/BasicGameApp.java
@@ -0,0 +1,18 @@
+package demo;
+
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.settings.GameSettings;
+
+public class BasicGameApp extends GameApplication {
+
+    @Override
+    protected void initSettings(GameSettings settings) {
+        settings.setWidth(800);
+        settings.setHeight(600);
+        settings.setTitle("Basic Game App");
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
