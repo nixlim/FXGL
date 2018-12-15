89cd8569598865d1f2102796a26be9aa727cfaf8
Igor Ryabchuk <riabtchuk@gmail.com>
2018-12-15 02:05:47 +0000

    IPOC-0001 First Commit

--- src/main/java/demo/BasicGameApp.java
+++ src/main/java/demo/BasicGameApp.java
@@ -1,9 +1,23 @@
 package demo;
 
 import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.entity.GameWorld;
 import com.almasb.fxgl.settings.GameSettings;
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.entity.Entities;
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.input.Input;
+import com.almasb.fxgl.input.UserAction;
+import com.almasb.fxgl.settings.GameSettings;
+import javafx.scene.input.KeyCode;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+import java.util.Map;
 
 public class BasicGameApp extends GameApplication {
+
+  private Entity player;
 
     @Override
     protected void initSettings(GameSettings settings) {
@@ -15,4 +29,17 @@ public class BasicGameApp extends GameApplication {
     public static void main(String[] args) {
         launch(args);
     }
+
+    @Override
+    protected void initGame() {
+      player = Entities.builder()
+          .at(300,300)
+          .viewFromNode(new Rectangle(25, 25, Color.BLUE))
+          .buildAndAttach(getGameWorld());
+
+      player.translate(200,200);
+    }
+
+    @Override
+
 }
